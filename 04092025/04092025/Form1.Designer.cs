4bf64fe37bf19d04a115a25f73e679cf28d62fc9
Maria Sofia Londoño <167258979+Sof1aaa@users.noreply.github.com>
2025-09-04 11:24:15 -0500

    Add files via upload

---
+++ 04092025/04092025/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace _04092025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnRes = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.txtOp = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnSumatoria = new System.Windows.Forms.Button();
+            this.btnFac = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.Location = new System.Drawing.Point(190, 126);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(123, 23);
+            this.btnSum.TabIndex = 4;
+            this.btnSum.Text = "Suma";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnRes
+            // 
+            this.btnRes.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRes.Location = new System.Drawing.Point(357, 126);
+            this.btnRes.Name = "btnRes";
+            this.btnRes.Size = new System.Drawing.Size(117, 23);
+            this.btnRes.TabIndex = 5;
+            this.btnRes.Text = "Resta";
+            this.btnRes.UseVisualStyleBackColor = true;
+            this.btnRes.Click += new System.EventHandler(this.btnRes_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.Location = new System.Drawing.Point(190, 174);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(123, 23);
+            this.btnMul.TabIndex = 6;
+            this.btnMul.Text = "Multiplicación";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(357, 174);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(117, 23);
+            this.btnDiv.TabIndex = 7;
+            this.btnDiv.Text = "División";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Número 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(541, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Opcional";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(28, 73);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(190, 73);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(357, 73);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(100, 20);
+            this.txtRes.TabIndex = 2;
+            // 
+            // txtOp
+            // 
+            this.txtOp.Location = new System.Drawing.Point(521, 73);
+            this.txtOp.Name = "txtOp";
+            this.txtOp.Size = new System.Drawing.Size(100, 20);
+            this.txtOp.TabIndex = 3;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(278, 295);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(117, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnSumatoria
+            // 
+            this.btnSumatoria.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumatoria.Location = new System.Drawing.Point(357, 221);
+            this.btnSumatoria.Name = "btnSumatoria";
+            this.btnSumatoria.Size = new System.Drawing.Size(117, 23);
+            this.btnSumatoria.TabIndex = 9;
+            this.btnSumatoria.Text = "Sumatoria";
+            this.btnSumatoria.UseVisualStyleBackColor = true;
+            this.btnSumatoria.Click += new System.EventHandler(this.btnSumatoria_Click);
+            // 
+            // btnFac
+            // 
+            this.btnFac.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFac.Location = new System.Drawing.Point(190, 221);
+            this.btnFac.Name = "btnFac";
+            this.btnFac.Size = new System.Drawing.Size(123, 23);
+            this.btnFac.TabIndex = 10;
+            this.btnFac.Text = "Factorial";
+            this.btnFac.UseVisualStyleBackColor = true;
+            this.btnFac.Click += new System.EventHandler(this.btnFac_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(692, 356);
+            this.Controls.Add(this.btnFac);
+            this.Controls.Add(this.btnSumatoria);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtOp);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnRes);
+            this.Controls.Add(this.btnSum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnRes;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.TextBox txtOp;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnSumatoria;
+        private System.Windows.Forms.Button btnFac;
+    }
+}
+
---
+++ 04092025/04092025/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04092025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            txtRes.Text = Convert.ToString(Suma(num1, num2));
+
+        }
+
+        static int Suma (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            txtRes.Text = Convert.ToString(Resta(num1,num2));
+        }
+
+        static int Resta (int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            txtRes.Text = Convert.ToString(Multiplicacion(num1, num2));
+        }
+
+        static int Multiplicacion(int num1, int num2)
+        { 
+            return num1 * num2; 
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            if (num2 == 0)
+            {
+                MessageBox.Show("No se puede dividir por 0", "Error");
+                txtNum2.Clear();
+            }
+            else
+            {
+                int res = num1 / num2;
+                txtRes.Text = Convert.ToString(res);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFac_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int fac = 1, res;
+            if (num1 < 0)
+            {
+                MessageBox.Show("No se puede obtener factorial de numeros negativos", "Error");
+            }
+            else if (num1 == 0)
+            {
+                res = 1;
+                txtOp.Text = Convert.ToString(res);
+            }
+                txtOp.Text = Convert.ToString(Factorial(num1, fac));
+        }
+
+        static int Factorial (int num1, int fac)
+        {
+            for (int i = 1; i <= num1; i++)
+            {
+                fac *= i;
+            }
+            return fac;
+        }
+
+        private void btnSumatoria_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int sum=0;
+            txtOp.Text = Convert.ToString(Sumatoria(num1, sum));
+        }
+
+        static int Sumatoria(int num1, int sum)
+        {
+            for (int i = 1; i <= num1; i++)
+            { 
+            sum += i;
+            }
+            return sum;
+        }
+
+
+    }
+}
